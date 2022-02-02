32f86808a15514964c12d7d43f144fb4456ef565
Alexander <70067346+rosjy@users.noreply.github.com>
2022-02-02 13:37:36 +0100

    added on/off switch

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,53 +2,68 @@ function getElement(selector) {
     return document.querySelector(selector);
 }
 
+function createMessage(message) {
+    const messageElement = document.createElement("p");
+    messageElement.innerHTML = message;
+    list.append(messageElement);
+}
+
+function changeControllerHandler() {
+    console.log(window.localStorage.getItem("controller_checked"));
+    window.localStorage.setItem("controller_checked", controller.checked);
+}
+
 const list = getElement(".list");
+const controller = getElement("#switch");
 
-const data = chrome.storage.local.get("data", (data) => {
-    console.log(data);
-    console.log(JSON.parse(data.data));
-    const response = JSON.parse(data.data);
-    if (!response.results) {
-        const message = document.createElement('p')
-        message.innerHTML = 'no results...'
-        list.append(message)
-        return;
-    }
-    const items = response.results;
-    let itemsCount = 0;
-    console.log(items);
-    for (let item of items) {
-        const itemElement = document.createElement("li");
-        const itemWrapper = document.createElement("a");
-        const imageWrapper = document.createElement("div");
-        const contentWrapper = document.createElement("div");
-        //const imageElement = document.createElement("img");
-        const title = document.createElement("h4");
-        const year = document.createElement("h5");
+controller.checked =
+    window.localStorage.getItem("controller_checked") === "true";
 
-        imageWrapper.className = "list-item__img";
-        imageWrapper.style.backgroundImage = `url(${
-      item.image ? item.image.url : "../i/no-img.png"
-    }`;
-        contentWrapper.className = "list-item__content";
-        itemWrapper.setAttribute("target", "_blank");
-        itemWrapper.setAttribute("href", `https://www.imdb.com${item.id}`);
-        title.innerHTML = item.title;
-        year.innerHTML = item.year;
-        // imageElement.setAttribute(
-        //     "src",
-        //     item.image ? item.image.url : "../i/no-img.png"
-        // );
+let itemsCount = 0;
+const itemsMaxCount = 7
 
-        //imageWrapper.append(imageElement);
-        contentWrapper.append(title);
-        contentWrapper.append(year);
-        itemWrapper.append(imageWrapper);
-        itemWrapper.append(contentWrapper);
-        itemElement.append(itemWrapper);
+controller.addEventListener("change", changeControllerHandler);
 
-        list.append(itemElement);
-        if (++itemsCount > 7) break;
-        //list.append();
-    }
-});
+if (controller.checked) {
+    chrome.storage.local.get("data", ({ data }) => {
+        const response = JSON.parse(data);
+        if (!response.results) {
+            createMessage("no results...");
+            return;
+        }
+        const items = response.results;
+
+        for (let item of items) {
+            const itemElement = document.createElement("li");
+            const itemWrapper = document.createElement("a");
+            const imageWrapper = document.createElement("div");
+            const contentWrapper = document.createElement("div");
+            const title = document.createElement("h4");
+            const year = document.createElement("h5");
+            //set classes
+            imageWrapper.className = "list-item__img";
+            contentWrapper.className = "list-item__content";
+            //set attributes
+            itemWrapper.setAttribute("target", "_blank");
+            itemWrapper.setAttribute("href", `https://www.imdb.com${item.id}`);
+            //set styling
+            imageWrapper.style.backgroundImage = `url(${
+        item.image ? item.image.url : "../i/no-img.png"
+      }`;
+            //set values
+            title.innerHTML = item.title;
+            year.innerHTML = item.year;
+
+            contentWrapper.append(title);
+            contentWrapper.append(year);
+            itemWrapper.append(imageWrapper);
+            itemWrapper.append(contentWrapper);
+            itemElement.append(itemWrapper);
+            list.append(itemElement);
+
+            if (++itemsCount > itemsMaxCount) break;
+        }
+    });
+} else {
+    createMessage("Switch on an extension.");
+}
